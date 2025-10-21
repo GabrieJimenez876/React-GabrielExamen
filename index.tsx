f7b697f7bcdc4ca6158ac133b90e98291519f92c
null <null>
2025-10-21 09:17:38 -0400

    agrego html

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import "./tailwind.css" // Asegúrate de que esto esté configurado
+import "./tailwind.css"
 import App from "./App"
 
 createRoot(document.getElementById('root')!).render(<App />)
